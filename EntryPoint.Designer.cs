cdde9dc17496c8097f7e706106e279f34672b624
Weeko1 <venalouiemt@gmail.com>
2025-05-17 13:59:39 +0800

    Entrypoint design, Sql connected in Login Tab and validation included. Database still does not have input from users. Signup to be done next

--- EntryPoint.Designer.cs
+++ EntryPoint.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            labelLogin = new Label();
+            labelPassword = new Label();
+            btnLogin = new Button();
+            labeldonthaveaccount = new Label();
+            btnSignup = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(129, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 15);
+            label1.TabIndex = 0;
+            label1.Text = "VMS LOGO";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(65, 124);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(214, 23);
+            txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(63, 214);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(214, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // labelLogin
+            // 
+            labelLogin.AutoSize = true;
+            labelLogin.Location = new Point(65, 106);
+            labelLogin.Name = "labelLogin";
+            labelLogin.Size = new Size(37, 15);
+            labelLogin.TabIndex = 3;
+            labelLogin.Text = "Login";
+            // 
+            // labelPassword
+            // 
+            labelPassword.AutoSize = true;
+            labelPassword.Location = new Point(63, 196);
+            labelPassword.Name = "labelPassword";
+            labelPassword.Size = new Size(57, 15);
+            labelPassword.TabIndex = 4;
+            labelPassword.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(129, 309);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Log in";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // labeldonthaveaccount
+            // 
+            labeldonthaveaccount.AutoSize = true;
+            labeldonthaveaccount.Location = new Point(65, 262);
+            labeldonthaveaccount.Name = "labeldonthaveaccount";
+            labeldonthaveaccount.Size = new Size(131, 15);
+            labeldonthaveaccount.TabIndex = 6;
+            labeldonthaveaccount.Text = "Don't have an account?";
+            // 
+            // btnSignup
+            // 
+            btnSignup.AutoSize = true;
+            btnSignup.Font = new Font("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            btnSignup.ForeColor = Color.CornflowerBlue;
+            btnSignup.Location = new Point(202, 262);
+            btnSignup.Name = "btnSignup";
+            btnSignup.Size = new Size(73, 15);
+            btnSignup.TabIndex = 7;
+            btnSignup.Text = "Sign up here";
+            btnSignup.Click += labelSignup_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(340, 450);
+            Controls.Add(btnSignup);
+            Controls.Add(labeldonthaveaccount);
+            Controls.Add(btnLogin);
+            Controls.Add(labelPassword);
+            Controls.Add(labelLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Label labelLogin;
+        private Label labelPassword;
+        private Button btnLogin;
+        private Label labeldonthaveaccount;
+        private Label btnSignup;
     }
 }
--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,10 +1,39 @@
+using System.Data.SqlClient;
+
 namespace VehicleMaintenanceSystem
 {
     public partial class Form1 : Form
     {
+        private readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Louie\source\repos\wilu-git\VehicleMaintenanceSystem\UserDatabase.mdf;Integrated Security=True;";
         public Form1()
         {
             InitializeComponent();
+        }
+        
+
+        private void labelSignup_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using var conn = new SqlConnection(connectionString); 
+            conn.Open();
+            string query = "SELECT COUNT(*) FROM Users WHERE Username = @username AND Password = @password";
+            using var cmd = new SqlCommand(query, conn); 
+
+            cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+            int count = (int)cmd.ExecuteScalar();
+            if (count > 0)
+            {
+                MessageBox.Show("Login successful!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials.");
+            }
         }
     }
 }
